cb8f939a691694377b9d6d525fd8d0bc65d7cbf9
KishorRShetty <kishhlinuxshetty@gmail.com>
2021-12-14 18:08:26 +0000

    commit

--- nodeBackend/index.js
+++ nodeBackend/index.js
@@ -4,4 +4,16 @@ import dotenv from "dotenv"
 
 dotenv.config();
 const MongoClient = mongodb.MongoClient;
-const port = process.env.PORT;
+const port = process.env.PORT || 8000;
+
+MongoClient.connect(
+    process.env.DB_URI,
+    {
+        maxPoolSize:50,
+        wtimeoutMS: 2500
+    }
+)
+
+.catch(err=>{
+    console.error(err.stack);
+})
